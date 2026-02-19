8c5e5f37e13d23f411ab2c0d4a7391d4563a6d71
SUMAN MIAH <web.sumanmiah@gmail.com>
2026-02-19 21:52:15 +0600

    added home page with functional.

---
+++ script/cashout.js
@@ -0,0 +1,6 @@
+document.getElementById("cashout-btn").addEventListener("click", function () {
+    const agentNumber = document.getElementById("cashout-number").value;
+    const amount = document.getElementById("cashout-amount").value;
+    const pin = document.getElementById("input-pin").value;
+    
+});
